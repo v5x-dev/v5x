4c6091c32551a6a3ed7af57e48338bbfcc111a82
Qainguin <slimaster14@gmail.com>
2026-05-19 20:14:07 -0500

    Update Version and add coloring

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env bun
 
 import packageJson from "../package.json";
+import pc from "picocolors";
 
-console.log(`v5x ${packageJson.version}`);
+console.log(pc.redBright(`v5x ${packageJson.version}`));
+console.log("modern cli for v5 development");
